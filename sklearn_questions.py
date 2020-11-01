ce72cb50c0f18e25c822dd6b5786837d99584e4c
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2020-11-01 14:53:10 +0100

    init

---
+++ sklearn_questions.py
@@ -0,0 +1,35 @@
+import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_is_fitted
+from sklearn.utils.validation import check_array
+
+
+class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
+    """Write docstring
+    """
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        """Write docstring
+        """
+        X, y = check_X_y(X, y)
+        self.classes_ = np.unique(y)
+        # XXX fix
+        return self
+
+    def predict(self, X):
+        """Write docstring
+        """
+        check_is_fitted(self)
+        X = check_array(X)
+        y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
+        # XXX fix
+        return y_pred
+
+    def score(self, X, y):
+        """Write docstring
+        """
+        X, y = check_X_y(X, y)
+        y_pred = self.predict(X)
+        return np.mean(y_pred == y)
