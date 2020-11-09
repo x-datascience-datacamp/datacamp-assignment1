5e7e3fbc7db6600e9633c35483edaacc0925aab8
BadrAlpha07 <badrbetta07@gmail.com>
2020-11-09 12:26:57 +0100

    My results

--- numpy_questions.py
+++ numpy_questions.py
@@ -26,8 +26,16 @@ def max_index(X):
     """
     i = 0
     j = 0
-
+    
+    
     # TODO
+    if(type(X) is not np.ndarray):
+        raise TypeError("X isn't a numpy array")     
+    elif(X.shape != (2,2)): 
+        raise Exception("the array shape is not 2D")           
+    else:
+        i = np.argmax(np.max(X, axis=1))
+        j = np.argmax(np.max(X, axis=0))          
 
     return i, j
 
@@ -37,10 +45,28 @@ def wallis_product(n_terms):
 
     See:
     https://en.wikipedia.org/wiki/Wallis_product
+    
+    Return the index of the maximum in a numpy array.
 
-    XXX : write Parameters and Returns sections as above.
+    Parameters
+    ----------
+    n_terms : integer
+        The n_terms is an int that corresponds to the number of terms in the product. For example 10000.
 
-    """
+    Returns
+    -------
+    pi : float
+        pi value approximation
+
+    """    
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    pi_2 = 1.
+    pi_n = 0.0
+
+    for n in range(1,n_terms+1):
+        pi_n = (4.0 * n**2)/(4.0 * n**2 - 1.0)
+        pi_2 = pi_2 * pi_n
+    pi = pi_2 * 2    
+    
+    return pi
