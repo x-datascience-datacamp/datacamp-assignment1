afa0c61788ee607f9c160cc4b2bd078a566b4a2a
celineboegler <celine.boegler@gmail.com>
2020-11-09 20:34:41 +0100

    premier commit

--- numpy_questions.py
+++ numpy_questions.py
@@ -27,8 +27,14 @@ def max_index(X):
     i = 0
     j = 0
 
-    # TODO
-
+    if type(X) is not np.ndarray:
+        raise ValueError('The input is not a numpy array')
+    if X.ndim != 2:
+        raise ValueError('The input array is note in 2D')
+    i, j = np.where(X == X.max())
+    if len(i) == 2:
+        j = i[1]
+        i = i[0]
     return i, j
 
 
@@ -38,9 +44,27 @@ def wallis_product(n_terms):
     See:
     https://en.wikipedia.org/wiki/Wallis_product
 
-    XXX : write Parameters and Returns sections as above.
+    Parameters
+    ----------
+    n_term : int
+            The n_terms is an int that corresponds to the number of
+            terms in the product. For example 10000.
 
+    Returns
+    -------
+    pi_n : int
+        The row index of the maximum.
+
+    Raises
+    ------
+    ValueError
+        If the input is not an integer
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    if n_terms - abs(n_terms) != 0:
+        raise ValueError('The input is not an integer')
+
+    pi_n = 1
+    for i in range(1, n_terms+1):
+        n_2 = 2*i
+        pi_n *= n_2**2/((n_2-1)*(n_2+1))
+    return 2*pi_n
--- sklearn_questions.py
+++ sklearn_questions.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
 from sklearn.utils.validation import check_array
+from sklearn.utils.multiclass import check_classification_targets
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
@@ -12,24 +13,27 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         pass
 
     def fit(self, X, y):
-        """Write docstring
+        """Pretraitement des données
         """
         X, y = check_X_y(X, y)
+        check_classification_targets(y)
         self.classes_ = np.unique(y)
-        # XXX fix
+        self.X_train_ = X
+        self.y_train_ = y
         return self
 
     def predict(self, X):
-        """Write docstring
+        """Prédiction de la classe des données X
         """
         check_is_fitted(self)
         X = check_array(X)
         y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
-        # XXX fix
+        nearest = [np.argmin(np.linalg.norm(self.X_train_-x, axis=1)) for x in X]
+        y_pred = np.array([self.y_train_[n] for n in nearest])
         return y_pred
 
     def score(self, X, y):
-        """Write docstring
+        """Donne l'erreur de la classification prédite sur les data X
         """
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
