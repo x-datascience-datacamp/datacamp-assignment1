41590083d8786ad601e35e50bc21265b2b8c0020
Yassine <yassine.filali@ensta-paris.fr>
2020-11-09 18:24:40 +0100

    first assignment

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -7,13 +7,12 @@ from sklearn.utils.multiclass import check_classification_targets
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
+    """1-NN class."""
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """Write docstring
-        
-        """
+        """Write docstring."""
         X, y = check_X_y(X, y)
         check_classification_targets(y)
         self.classes_ = np.unique(y)
@@ -23,9 +22,7 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         return self
 
     def predict(self, X):
-        """Write docstring
-        
-        """
+        """Write docstring."""
         check_is_fitted(self)
         X = check_array(X)
         y_pred = np.full(shape=len(X), fill_value=self.classes_[0],
@@ -39,9 +36,7 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         return y_pred
 
     def score(self, X, y):
-        """Write docstring
-        
-        """
+        """Write docstring."""
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
         return np.mean(y_pred == y)
