9b816e3acb2a5beca018dfcecc5aef207a89037c
Jérôme Bonnin <bonnin.j49@gmail.com>
2020-11-09 18:52:39 +0100

    wrote correct code and add exceptions

--- numpy_questions.py
+++ numpy_questions.py
@@ -24,10 +24,12 @@ def max_index(X):
         If the input is not a numpy error or
         if the shape is not 2D.
     """
-    
-    idx = np.unravel_index(np.argmax(X), X.shape)
-    i = idx[0]
-    j = idx[1]
+    if isinstance(X, np.ndarray) and X.ndim == 2:
+        idx = np.unravel_index(np.argmax(X), X.shape)
+        i = idx[0]
+        j = idx[1]
+    else:
+        raise ValueError('ValueError')
 
     return i, j
 
@@ -46,7 +48,7 @@ def wallis_product(n_terms):
     Returns
     -------
     wp : float
-        The resulting Wallis product (multiplied by 2 to get a pi approximation).
+        The resulting Wallis product (doubled to get a pi approximation).
 
     Raises
     ------
@@ -55,9 +57,8 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    
-    x = np.arange(1, n_terms+1)
-    y = 4 * x**2
-    wp = 2 * np.prod(y / (y - 1))
 
-    return wp
+    y = np.array([4 * x**2 / (4 * x**2 - 1) for x in range(1, n_terms+1)])
+    z = np.prod(y)
+
+    return 2 * z
--- sklearn_questions.py
+++ sklearn_questions.py
@@ -2,6 +2,7 @@
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
+from sklearn.utils.multiclass import check_classification_targets
 from sklearn.utils.validation import check_array
 
 
@@ -17,6 +18,9 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         X, y = check_X_y(X, y)
         self.classes_ = np.unique(y)
         # XXX fix
+        check_classification_targets(y)
+        self.X_ = X
+        self.y_ = y
         return self
 
     def predict(self, X):
@@ -26,6 +30,8 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         X = check_array(X)
         y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
         # XXX fix
+        y_pred = np.array([
+            self.y_[np.argmin(np.linalg.norm(x - self.X_, axis=1))]for x in X])
         return y_pred
 
     def score(self, X, y):
