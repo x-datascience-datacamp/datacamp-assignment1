24d25fd39cc487d4586925af80351d9f3bc6e61c
Théo Dullin <theo.dullin@ensta-paris.fr>
2020-11-09 17:22:37 +0100

    flake8 update

--- numpy_questions.py
+++ numpy_questions.py
@@ -30,18 +30,18 @@ def max_index(X):
     if type(X) != np.ndarray:
         raise ValueError("Input is not a Numpy Array")
 
-    if X.ndim!=2 :
+    if X.ndim != 2:
         raise ValueError("Input is a Numpy array but must be 2D")
 
-    n,m=X.shape
-    maxi=X[0][0]
+    n, m = X.shape
+    maxi = X[0][0]
 
     for row in range(n):
         for col in range(m):
-            if X[row,col]>maxi:
-                i=row
-                j=col
-                maxi=X[row,col]
+            if X[row, col] > maxi:
+                i = row
+                j = col
+                maxi = X[row, col]
 
     return i, j
 
@@ -57,8 +57,8 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    res=2.
-    for n in range(1,n_terms+1):
-        res*=4*n**2/(4*n**2-1)
+    res = 2.
+    for n in range(1, n_terms+1):
+        res *= 4*n**2/(4*n**2-1)
 
     return res
