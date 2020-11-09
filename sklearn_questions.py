9b77e9b6c45f771fb8bc53d06b9682134d710c09
gouzmi <guillaume.dupont.rm@gmail.com>
2020-11-09 14:54:27 +0100

    sklearn questions

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -3,34 +3,68 @@ import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
 from sklearn.utils.validation import check_array
+from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.metrics import f1_score
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
     """Write docstring
     """
+
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """Write docstring
+        """Fit the estimator
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            The input data array.
+        y : ndarray of shape (n_samples,)
+            the true labels for X
+        Returns
+        -------
+        self : OneNearestNeighbor() instance of the classifier
         """
+
         X, y = check_X_y(X, y)
         self.classes_ = np.unique(y)
-        # XXX fix
+
+        self.X_ = X
+        self.y_ = y
+
         return self
 
     def predict(self, X):
-        """Write docstring
+        """Predict the given data X based on self.X
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            The input data array.
+        Returns
+        -------
+        y_pred : the predicted labels for X
         """
         check_is_fitted(self)
         X = check_array(X)
         y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
-        # XXX fix
+
+        distances = euclidean_distances(X, self.X_)
+        distances = [np.argmin(distances[i]) for i in range(distances.shape[0])]
+        y_pred = self.y_[distances]
+
         return y_pred
 
     def score(self, X, y):
-        """Write docstring
+        """Compute the f1 score for a given data and base prediction.
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            The input data array.
+        y : ndarray of shape (n_samples,)
+            the true labels for X
+        Returns
+        -------
+        score: float f1 score.
         """
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
-        return np.mean(y_pred == y)
+        return f1_score(y,y_pred)
