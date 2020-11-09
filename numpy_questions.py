4de3265741453390d4851b09edd082e9d244da7b
MixalisThomas <mixalisthomas8@gmail.com>
2020-11-09 22:46:25 +0200

    Assignment1

--- numpy_questions.py
+++ numpy_questions.py
@@ -28,7 +28,7 @@ def max_index(X):
 
     # TODO
     if(isinstance(X, np.ndarray) and X.ndim == 2):
-        i,j = np.unravel_index(np.argmax(X, axis=None), X.shape)
+        i, j = np.unravel_index(np.argmax(X, axis=None), X.shape)
     else:
         raise ValueError("Error!")
     return i, j
@@ -45,10 +45,12 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    pi = 2.
-    for i in range(1, n_terms):
-        left = (2. * i)/(2. * i - 1.)
-        right = (2. * i)/(2. * i + 1.)
-        pi = pi * left * right
-
-    return pi
+    if(n_terms < 0):
+        raise ValueError("Error!")
+    elif(not isinstance(n_terms, int)):
+        raise ValueError("Error!")
+    else:
+        halfpi = 1.
+        for i in range(1, n_terms+1):
+            halfpi *= 4 * i**2 / (4*(i**2)-1)
+        return 2 * halfpi
--- sklearn_questions.py
+++ sklearn_questions.py
@@ -1,8 +1,10 @@
 # noqa: D100
 import numpy as np
+from sklearn.utils.multiclass import check_classification_targets
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
 from sklearn.utils.validation import check_array
+from sklearn.metrics import euclidean_distances
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
@@ -16,7 +18,10 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         """
         X, y = check_X_y(X, y)
         self.classes_ = np.unique(y)
-        # XXX fix
+        check_classification_targets(self.classes_)
+        self.X_ = X
+        self.y_ = y
+        
         return self
 
     def predict(self, X):
@@ -25,7 +30,7 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         check_is_fitted(self)
         X = check_array(X)
         y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
-        # XXX fix
+        y_pred = self.y_[np.argmin(euclidean_distances(X, self.X_), axis=1)]
         return y_pred
 
     def score(self, X, y):
