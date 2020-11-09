4900c9bf508e48e7ac75d68f680e1fc394bcd3f7
bechir-tr <bechir.trabelsi@ensta-paris.fr>
2020-11-09 12:26:12 +0100

    My code for the numpy questions

--- numpy_questions.py
+++ numpy_questions.py
@@ -24,12 +24,22 @@ def max_index(X):
         If the input is not a numpy error or
         if the shape is not 2D.
     """
+
+    
+    if type(X) is not np.ndarray:
+        raise ValueError("not numpy")
+    if len(X.shape)!=2:
+        raise ValueError("dim not 2D")
     i = 0
     j = 0
-
+    
     # TODO
+    (i,j)=np.where(X==np.max(X)) 
+    (i,j)=(i[0],j[0])
+       
+    return i,j
 
-    return i, j
+    
 
 
 def wallis_product(n_terms):
@@ -43,4 +53,11 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    try:
+        n_terms=int(n_terms)
+    except ValueError:
+        raise ValueError("n_terms must be integer")
+    pi=1
+    for n in range(1,n_terms+1):
+        pi=pi*(4*n**2)/(4*n**2 - 1)
+    return 2*pi
