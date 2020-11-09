774d0a5295cd59af9a70238dff9dd2ab304e7841
jawadhussein462 <jawadhussein462@gmail.com>
2020-11-09 13:58:34 +0100

    Testing

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -2,7 +2,10 @@
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
+from sklearn.utils.multiclass import check_classification_targets
 from sklearn.utils.validation import check_array
+from sklearn.metrics import euclidean_distances
+
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
@@ -15,16 +18,23 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         """Write docstring
         """
         X, y = check_X_y(X, y)
+        check_classification_targets(y)
         self.classes_ = np.unique(y)
         # XXX fix
+        self.X_ = X
+        self.y_ = y
         return self
 
     def predict(self, X):
         """Write docstring
         """
-        check_is_fitted(self)
+        check_is_fitted(self, ['X_', 'y_', 'classes_'])
         X = check_array(X)
-        y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
+
+        distance = euclidean_distances(X, self.X_)
+        indices = np.argmin(distance,axis=1)
+        y_pred=self.y_[indices]
+        #y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
         # XXX fix
         return y_pred
 
