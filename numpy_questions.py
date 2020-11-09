03168fbd396fce83aad31241098661427f6cc051
bechir-tr <bechir.trabelsi@ensta-paris.fr>
2020-11-09 13:50:21 +0100

    my code v2

--- numpy_questions.py
+++ numpy_questions.py
@@ -1,7 +1,5 @@
 # noqa: D100
 import numpy as np
-
-
 def max_index(X):
     """Return the index of the maximum in a numpy array.
 
@@ -24,19 +22,15 @@ def max_index(X):
         If the input is not a numpy error or
         if the shape is not 2D.
     """
-
-    
     if type(X) is not np.ndarray:
         raise ValueError("not numpy")
     if len(X.shape)!=2:
         raise ValueError("dim not 2D")
     i = 0
     j = 0
-    
     # TODO
     (i,j)=np.where(X==np.max(X)) 
     (i,j)=(i[0],j[0])
-       
     return i,j
 
     
@@ -46,7 +40,7 @@ def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
 
     See:
-    https://en.wikipedia.org/wiki/Wallis_product
+    https://en.wikipedia.org/wiki/Wallis_product.
 
     XXX : write Parameters and Returns sections as above.
 
