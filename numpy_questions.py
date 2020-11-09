efd8161355133d4726055564a8ced4379783b169
Rania <rania.ferchichi@telecom-paris.fr>
2020-11-09 12:23:57 +0100

    numpy_questions

--- numpy_questions.py
+++ numpy_questions.py
@@ -26,9 +26,12 @@ def max_index(X):
     """
     i = 0
     j = 0
-
     # TODO
-
+    if not isinstance(X, np.ndarray) or X is None or len(X.shape) !=2:
+        raise ValueError("input must be a 2D-numpy array")
+    
+    i, j = np.unravel_index(np.argmax(X, axis=None), X.shape)
+    
     return i, j
 
 
@@ -39,8 +42,21 @@ def wallis_product(n_terms):
     https://en.wikipedia.org/wiki/Wallis_product
 
     XXX : write Parameters and Returns sections as above.
+    Parameters
+    ----------
+    n_terms : int
+              number of terms in the product
 
+    Returns
+    -------
+    pi_approx : float
+                An approximation of pi using the Wallis product
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    pi = 2.
+    for i in range(1, n_terms+1):
+        left = (2.0 * i)/(2 * i - 1)
+        right = (2.0 * i)/(2 * i + 1)
+        pi = pi * left * right
+    return pi
