4fd4e1592746de68e8d470e14eaa36da44cf2c68
Ulysse Demay <u.demay@gmail.com>
2020-11-09 12:19:13 +0100

    numpy questions solved

--- numpy_questions.py
+++ numpy_questions.py
@@ -21,13 +21,24 @@ def max_index(X):
     Raises
     ------
     ValueError
-        If the input is not a numpy error or
+        If the input is not a numpy array or
         if the shape is not 2D.
     """
     i = 0
     j = 0
 
-    # TODO
+    if type(X) != np.ndarray:
+        raise ValueError("Wrong type")
+    if len(X.shape) != 2:
+        raise ValueError("Wrong dimension")
+
+    maximum = X[i][j]
+    for line in range(X.shape[0]):
+        for column in range(X.shape[1]):
+            if X[line][column] > maximum:
+                maximum = X[line][column]
+                i = line
+                j = column
 
     return i, j
 
@@ -43,4 +54,9 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+
+    resu = 2.
+    for i in range(1, n_terms+1):
+        resu *= (2*i / (2*i-1)) * (2*i / (2*i+1))
+
+    return resu
