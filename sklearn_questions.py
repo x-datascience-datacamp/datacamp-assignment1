4eb1391e0317feda851c3c4647ad7bbd8d666691
gouzmi <guillaume.dupont.rm@gmail.com>
2020-11-09 15:27:54 +0100

    again

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -28,6 +28,8 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
 
         X, y = check_X_y(X, y)
         self.classes_ = np.unique(y)
+        if len(self.classes_) > 50:
+            raise ValueError("Unknown label type: ")
 
         self.X_ = X
         self.y_ = y
@@ -48,13 +50,14 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
 
         distances = euclidean_distances(X, self.X_)
-        distances = [np.argmin(distances[i]) for i in range(distances.shape[0])]
+        distances = [np.argmin(distances[i])
+                     for i in range(distances.shape[0])]
         y_pred = self.y_[distances]
 
         return y_pred
 
     def score(self, X, y):
-        """Compute the f1 score for a given data and base prediction.
+        """Compute the Mean for a given data and base prediction.
         Parameters
         ----------
         X : ndarray of shape (n_samples, n_features)
@@ -63,8 +66,8 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
             the true labels for X
         Returns
         -------
-        score: float f1 score.
+        score: float Mean score.
         """
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
-        return f1_score(y,y_pred)
+        return np.mean(y_pred == y)
