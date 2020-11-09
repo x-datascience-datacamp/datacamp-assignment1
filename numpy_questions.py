3a48ba4a87e7aef378b18a2bdfa1ccf03192a1d4
raphael <raphael.boige@telecom-paristech.fr>
2020-11-09 16:09:59 +0100

    first assignment

--- numpy_questions.py
+++ numpy_questions.py
@@ -24,23 +24,33 @@ def max_index(X):
         If the input is not a numpy error or
         if the shape is not 2D.
     """
-    i,j = np.argmax(X,axis=1)
-    return i,j
+    if type(X) != np.ndarray:
+        raise ValueError()
+    if len(X.shape) != 2:
+        raise ValueError()
 
+    n, d = X.shape
+    a = np.argmax(X)
+    i = a // d
+    j = a % d
+    return i, j
 
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
-
     See:
     https://en.wikipedia.org/wiki/Wallis_product
 
-    XXX : write Parameters and Returns sections as above.
+    Parameters
+    ----------
+    n_terms : int
+        The number of terms in the product
 
+    Returns
+    -------
+    pi_approx : int
+        An approximation of the number PI
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    A = np.array([4*n**2/(4*n**2-1) for n in range(1,n_terms)])
-    return A.prod(axis=0)
-
-
+    A = np.array([4*n**2/(4*n**2-1) for n in range(1, n_terms+1)])
+    pi_approx = 2*A.prod(axis=0)
+    return pi_approx
