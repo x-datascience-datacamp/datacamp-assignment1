5c1db713d58367469b2887d47d7734846f7beab2
Maxime Lepeytre <maxime.lepeytre@orange.fr>
2020-11-09 12:38:14 +0100

    test

--- numpy_questions.py
+++ numpy_questions.py
@@ -28,8 +28,13 @@ def max_index(X):
     j = 0
 
     # TODO
-
+    if type(X) is np.ndarray :
+        raise Exception("the input is not a numpy array")
+    if len(X.shape) != 2 :
+        raise Exception("the shape is not 2D")
+    i,j = np.argmax(X,axis=-1)
     return i, j
+
 
 
 def wallis_product(n_terms):
@@ -38,9 +43,28 @@ def wallis_product(n_terms):
     See:
     https://en.wikipedia.org/wiki/Wallis_product
 
-    XXX : write Parameters and Returns sections as above.
+    Parameters
+    ----------
+    n_terms : the number of terms for the approximation of pi.
 
+    Returns
+    -------
+    pi_approx : the approximate pi value with wallis product (n_terms times)
+    
+    Raises
+    ------
+    ValueError
+        If n_terms < 0 
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+
+    pi_approx = 1
+    if n_terms < 0:
+        raise ValueError("n_terms < 0")
+        
+    if n_terms == 0:
+        return 2.0*pi_approx
+        
+    for i in range(1,n_terms):
+        pi_approx *= ((4*i**2)/(4*i**2-1))
+        
+    return 2.0*pi_approx
