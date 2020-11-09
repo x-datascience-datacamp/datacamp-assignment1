4ba17e78b4fa4bf1345f00913f49814f8bbbb104
simondemouchy <simondemouchy@hotmail.fr>
2020-11-09 18:15:27 +0100

    ok

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
 from sklearn.utils.validation import check_array
+from sklearn.metrics import euclidean_distances
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
