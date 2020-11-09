5e13572f55fad9bc1244e1373909a8782ddb1d70
Nicolas Boussenina <nicolas.boussenina@jeece.fr>
2020-11-09 13:42:47 +0100

    sklearn

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.spatial import distance
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
+from sklearn.utils.multiclass import check_classification_targets
 from sklearn.utils.validation import check_array
 
 
@@ -27,6 +28,7 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
             The input array.
         
         """
+        check_classification_targets(y)
         X, y = check_X_y(X, y)
         self.classes_ = np.unique(y)
         self.X_ = X
@@ -37,8 +39,8 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         """
         Parameters
         ----------
-        X : ndarray of shape (n_samples, n_features)
-            The input array.
+        X : ndarray of shape (n_features)
+            The test input array.
         y : ndarray of shape (n_samples)
             The ground truth values.
 
@@ -49,12 +51,21 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         check_is_fitted(self)
         X = check_array(X)
         y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
-        D = distance.squareform(distance.pdist(X))
-        closest = np.argsort(D, axis=1)
-        closest = closest[:, 1]
-        print(closest)
-        y_pred = closest
+        neighbor_distances_and_indices = np.empty((len(X), len(self.X_)))
+
+        k_nearest_distances_and_indices = np.empty((len(X)))
+
+        for index, row_query in enumerate(X):
+            for index_row, row in enumerate(self.X_):
+                distance = np.linalg.norm(row - row_query)
+                neighbor_distances_and_indices[index][index_row] = distance
+
+        for index, row_query in enumerate(X):
+            k_nearest_distances_and_indices[index] = np.argmin(neighbor_distances_and_indices[index])
+        y_pred = np.array([self.y_[int(i)] for i in k_nearest_distances_and_indices])
+
         return y_pred
+
 
     def score(self, X, y):
         """
