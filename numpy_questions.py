539639e8fbd74724586fb384241d254a8205fcc9
Emma Sarfati <emmasarfati@MacBook-Pro-de-Emma.local>
2020-11-09 14:28:53 +0100

    work

--- numpy_questions.py
+++ numpy_questions.py
@@ -1,7 +1,6 @@
 # noqa: D100
 import numpy as np
 
-
 def max_index(X):
     """Return the index of the maximum in a numpy array.
 
@@ -28,9 +27,19 @@ def max_index(X):
     j = 0
 
     # TODO
+    if type(X) is not np.ndarray:
+        raise ValueError('The input is not a numpy array')
+    if len(X.shape) != 2:
+        raise ValueError('The shape is not 2D')
+
+    i = np.where(X == np.amax(X))[0][0]
+    j = np.where(X == np.amax(X))[1][0]
+
+    # END TODO
 
     return i, j
 
+from functools import reduce
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
@@ -43,4 +52,10 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+
+    a = [(4 * n**2) / (4 * n**2 - 1) for n in range(1,n_terms)]
+
+    if len(a)
+
+    return 2*reduce(lambda x,y: x*y, a)
+
