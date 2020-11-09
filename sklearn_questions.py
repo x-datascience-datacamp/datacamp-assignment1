7ed76ef85ac28d83f9fed9256950eb6600e56aa4
elgarrai <elgarrai.aymane@gmail.com>
2020-11-09 21:19:12 +0100

    solving flake8 problems

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -7,11 +7,14 @@ from sklearn.utils.multiclass import check_classification_targets
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-
+    """Write docstring
+    """
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
+        """Write docstring
+        """
         X, y = check_X_y(X, y)
         check_classification_targets(y)
         self.classes_ = np.unique(y)
@@ -23,6 +26,8 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         return self
 
     def predict(self, X):
+        """Write docstring
+        """
         check_is_fitted(self)
         X = check_array(X)
         y_pred = np.full(shape=len(X), fill_value=self.classes_[0], dtype=self.classes_.dtype)
@@ -37,6 +42,8 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         return y_pred
 
     def score(self, X, y):
+        """Write docstring
+        """
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
         return np.mean(y_pred == y)
