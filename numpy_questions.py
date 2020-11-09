9384f64d9e6b19a4bec0d0926746aca46d9ec9d2
Marquis <alexandre.marquis31@gmail.com>
2020-11-09 13:07:43 +0100

    everything should be working

--- numpy_questions.py
+++ numpy_questions.py
@@ -27,17 +27,18 @@ def max_index(x):
     i = 0
     j = 0
 
-    if type(x).__module__ == np.__name__:
+    if not isinstance(x,np.ndarray):
         raise ValueError("X is not an numpy array")
     if x.ndim != 2:
         raise ValueError("X shape is not 2D")
 
     m = x[0][0]
-    for x in range(0, x.shape[0]):
-        for y in range(0, x.shape[1]):
-            if x[x][y] > m:
-                i = x
-                j = y
+    for k in range(0, x.shape[0]):
+        for l in range(0, x.shape[1]):
+            if x[k][l] > m:
+                i = k
+                j = l
+                m = x[k][l]
 
     return i, j
 
@@ -53,9 +54,11 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
+    if n_terms == 0 :
+        return 2.0
 
     product = 1
-    for n in range(n_terms):
-        product *= 4*n**2 / (4*n**2 - 1)
+    for n in range(1,n_terms + 1):
+        product *= 4*n**2 / (4*n**2 - 1.)
 
-    return 0.
+    return product*2
