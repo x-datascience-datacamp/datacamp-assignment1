2a2773ff945d34a42bf3fa4584e9d654ecd7acad
ChristosKts <christos.katsoulakis@ensae.fr>
2020-11-09 19:08:06 +0100

    Assignement ChristosKts

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -8,6 +8,7 @@ from sklearn.utils.multiclass import check_classification_targets
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
     """Define class for the model One Nearest Neighbor."""
+
     def __init__(self):  # noqa: D107
         pass
 
