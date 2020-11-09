1038ba1259b294d9aa445b8fa609f77c935b9cea
Ophélie Bleu <opheliebleu@MacBook-Air-de-Ophelie.local>
2020-11-09 15:45:00 +0100

    fix errors

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
 from sklearn.utils.validation import check_array
+from sklearn.utils.multiclass import check_classification_targets
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
@@ -18,23 +19,23 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         """Fit X to y."""
         X, y = check_X_y(X, y)
         self.classes_ = np.unique(y)
-        if len(np.unique(y)) > 50:
-            raise ValueError("Regression")
+        check_classification_targets(self.classes_)
         # XXX fix
         self.sample_ = X
-        self.fit_ = y
+        self.y_ = y
         return self
 
     def predict(self, X):
         """Predict y with X."""
         check_is_fitted(self, ["classes_"])
         X = check_array(X)
-        y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
+        y_pred = np.full(shape=len(X), fill_value=self.classes_[0],
+                         dtype=self.classes_.dtype)
         # XXX fix
         for i in range(0, len(y_pred)):
             distances = np.sqrt(np.sum(np.square(X[i] - self.sample_),
                                 axis=-1, keepdims=True))
-            y_pred[i] = self.fit_[np.argmin(distances)]
+            y_pred[i] = self.y_[np.argmin(distances)]
         return y_pred
 
     def score(self, X, y):
