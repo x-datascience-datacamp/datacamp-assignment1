6cbfb74f410e9662d59980d164cf7dea8b0977df
sushifou <infernox78@gmail.com>
2020-11-09 14:02:43 +0100

    SkLearn exercize advancements

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -6,22 +6,55 @@ from sklearn.utils.validation import check_array
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    """Write docstring
+    """
+    One Nearest Neighbor Classifier  - Scikit Learn API
+
+    Author :  Yann KERVELLA
     """
 
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """Write docstring
+        """
+        Fit function of our One nearest neighbor model
+
+        Parameters
+        ----------
+        X : ndarray of training predictors (n_samples, n_features)
+        y : ndarray of training target values (n_samples)
+
+        Returns
+        -------
+        self : OneNearestNeighbor() class
+
+        Raises
+        ------
+        ValueError
+            Maximum number of classes has been reached, are you sure it is not regression problem
         """
         X, y = check_X_y(X, y)
         self.classes_ = np.unique(y)
-        # XXX fix
+        if len(self.classes) > 50:
+            raise ValueError("Maximum number of classes has been reached, are you sure it is not regression problem ?")
+        else:
+            # Model fitting
+            self.X_ = X
+            self.y_ = y
+
         return self
 
     def predict(self, X):
-        """Write docstring
+        """
+        Predict function of our One nearest neighbor model
+
+        Parameters
+        ----------
+        X : ndarray of test predictors (n_samples, n_features)
+
+        Returns
+        -------
+        y_pred = ndarray the predicted values (n_samples)
         """
         check_is_fitted(self)
         X = check_array(X)
@@ -30,7 +63,17 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         return y_pred
 
     def score(self, X, y):
-        """Write docstring
+        """
+        Score function of our One nearest neighbor model
+
+        Parameters
+        ----------
+        X : ndarray of test predictors (n_samples, n_features)
+        y : ndarray of test target values (n_samples)
+
+        Returns
+        -------
+        The accuracy of the predictions using a mean function.
         """
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
