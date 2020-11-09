355ce9ef7cb9623c9504536023cb08daaba38eba
benj-v <benjamin.vignal@ensae.fr>
2020-11-09 15:56:54 +0100

    commit3

--- numpy_questions.py
+++ numpy_questions.py
@@ -28,11 +28,11 @@ def max_index(X):
     j = 0
 
     # TODO
-    if type(X) is not np.ndarray :
+    if type(X) is not np.ndarray:
         raise ValueError('Not a numpy array')
-    if len(X.shape) != 2 :
+    if len(X.shape) != 2:
         raise ValueError('Shape is not 2D')
-    i,j = np.unravel_index(np.argmax(X, axis=None), X.shape)
+    i, j = np.unravel_index(np.argmax(X, axis=None), X.shape)
 
     return i, j
 
@@ -50,7 +50,7 @@ def wallis_product(n_terms):
     # terms in the product. For example 10000.
 
     wallis_product = 2
-    for i in range(1, n_terms + 1) :
+    for i in range(1, n_terms + 1):
         wallis_product = wallis_product * 4 * i**2 / (4 * i**2 - 1)
 
     return wallis_product
