3700636de9947b41b24b534a7166d496d4b5c066
LoicJOURDAN <loic.jourdan@ensae-paristech.fr>
2020-11-09 14:53:56 +0100

    Premier commit

--- numpy_questions.py
+++ numpy_questions.py
@@ -24,11 +24,14 @@ def max_index(X):
         If the input is not a numpy error or
         if the shape is not 2D.
     """
+    if type(X) is not np.ndarray:
+        raise ValueError('The input is not a numpy array')
+    if len(X.shape) != 2:
+        raise ValueError('The shape is not 2D')
     i = 0
     j = 0
-
-    # TODO
-
+    i = np.where(X==np.amax(X))[0][0]
+    j = np.where(X==np.amax(X))[1][0]
     return i, j
 
 
@@ -43,4 +46,7 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    a = [(4*n**2)/(4*n**2-1) for n in range((1,n_terms))]
+    rep = 2*reduce(lambda x,y:x*y,a)
+    return rep
+
