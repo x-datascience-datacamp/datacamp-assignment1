1ef37125b24d3493e1058ed09599a3b19ac1dc9c
Maroua MAACHOU <maroua.maachou@telecom-sudparis.eu>
2020-11-09 20:17:20 +0100

    sklearn_questions resolved

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
 from sklearn.utils.validation import check_array
+from sklearn.utils.multiclass import check_classification_targets
+
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
@@ -15,17 +17,28 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         """Write docstring
         """
         X, y = check_X_y(X, y)
+        check_classification_targets(y)
         self.classes_ = np.unique(y)
+        self.X_ = X
+        self.y_ = y
         # XXX fix
         return self
 
     def predict(self, X):
         """Write docstring
         """
+        if X is None:
+            raise ValueError
+
         check_is_fitted(self)
         X = check_array(X)
-        y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
+        arr_type = self.classes_.dtype
+        y_pred = np.full(shape=len(X), fill_value=self.classes_[0], dtype = arr_type)
         # XXX fix
+        
+        for index, x in enumerate(X):
+            distances = np.linalg.norm(self.X_ - x, axis=1)
+            y_pred[index] = self.y_[np.argmin(distances)]
         return y_pred
 
     def score(self, X, y):
