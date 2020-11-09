bb96ac01de543b3f810937d9c0742ef9c6c7f321
Théo Dullin <theo.dullin@ensta-paris.fr>
2020-11-10 00:46:06 +0100

    Assignment 1 final

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
 from sklearn.utils.validation import check_array
+from sklearn.utils.multiclass import check_classification_targets
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
@@ -37,6 +38,7 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
             Returns self.
         """
         X, y = check_X_y(X, y)
+        check_classification_targets(y)
         self.classes_ = np.unique(y)
         self.X_ = X
         self.y_ = y
@@ -59,10 +61,7 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         check_is_fitted(self)
         X = check_array(X)
         y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
-        for i in range(X.shape[0]):
-            dist = [np.linalg.norm(X[i, :] - self.X_[j, :], ord=2)
-                    for j in range(self.X_.shape[0])]
-            y_pred[i] = self.y_[np.argmin(dist)]
+        y_pred = np.array([self.y_[np.argmin(np.linalg.norm(x - self.X_, axis=1))]for x in X])
         return y_pred
 
     def score(self, X, y):
