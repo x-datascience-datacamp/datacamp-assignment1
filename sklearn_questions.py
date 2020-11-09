f89a625fe7ee1e862585e5613a4e37ef1bbe7b33
saif-fares <faressaif04@gmail.com>
2020-11-09 20:13:50 +0100

    Finishing the code for sklearn_questions

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -6,31 +6,33 @@ from sklearn.utils.validation import check_array
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    """Write docstring
-    """
+    """Implement a 1-NN class with functions fit, predit and score."""
+
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """Write docstring
-        """
+        """Train the data."""
         X, y = check_X_y(X, y)
         self.classes_ = np.unique(y)
-        # XXX fix
+        self.X_ = X.copy()
+        self.y_ = y.copy()
         return self
 
     def predict(self, X):
-        """Write docstring
-        """
+        """Predicting the results for X using 1-NN method."""
         check_is_fitted(self)
         X = check_array(X)
         y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
-        # XXX fix
+        
+        def distance(vector):
+            return ((self.X_ - vector)**2).sum(axis=1)
+        index = np.argmin(distance(X))
+        y_pred = self.y_[index]    
         return y_pred
 
     def score(self, X, y):
-        """Write docstring
-        """
+        """Calculate the score of the prediction over the data set X,y."""
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
         return np.mean(y_pred == y)
