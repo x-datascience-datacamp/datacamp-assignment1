f63910f2320b96294e42e6fe710a61eb865938c9
Mohamed-el-khames <Mohamed El Khames@KhAMES-LAPTOP>
2020-11-09 19:31:44 +0100

    commit5

--- numpy_questions.py
+++ numpy_questions.py
@@ -28,36 +28,30 @@ def max_index(X):
     j = 0
 
     # TODO
-    if not(isinstance(X,np.ndarray )):
+    if not(isinstance(X, np.ndarray)):
         raise ValueError('you should enter an array')
 
-    if len(X.shape)!=2:
+    if len(X.shape) != 2:
         raise ValueError('you should enter a 2-D array')
 
-    argmax=np.argmax(X)
-    i,j=np.unravel_index(argmax, X.shape)
+    argmax = np . argmax(X)
+    i, j = np.unravel_index(argmax, X.shape)
 
-    return i,j
-
-
+    return i, j
 
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
-
     See:
     https://en.wikipedia.org/wiki/Wallis_product
-
     XXX : write Parameters and Returns sections as above.
-
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    prod=2
-    for i in range(1,1+n_terms):
+    prod = 2
+    for i in range(1, 1 + n_terms):
         a = (2 * i)/(2 * i - 1)
         b = (2 * i)/(2 * i + 1)
         total = a * b
-        prod=prod*total
+        prod = prod * total
     return prod
-
--- sklearn_questions.py
+++ sklearn_questions.py
@@ -3,13 +3,11 @@ import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
 from sklearn.utils.validation import check_array
-from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.utils.multiclass import check_classification_targets
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
     """predection of the nearest neighbor to a given data."""
-    
     def __init__(self):  # noqa: D107
         pass
 
@@ -18,29 +16,26 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         X, y = check_X_y(X, y)
         check_classification_targets(y)
         self.classes_ = np.unique(y)
-        self.X_=X
-        self.y_=y
-        self.Y_=y
-        
+        self.X_ = X
+        self.y_ = y
+        self.Y_ = y
         # XXX fix
-        
         return self
 
     def predict(self, X):
         """Class prediction."""
+        # Manual method
         # check_is_fitted(self)
         # X = check_array(X)
-        
-        # y_pred = np.full(shape=len(X),dtype=self.classes_.dtype,fill_value=self.classes_[0])
-        #y_pred = np.full(shape=len(X),fill_value=self.classes_[0])
+        # y_pred = np.full(shape=len(X),dtype=self.classes_.dtype,
+        # fill_value=self.classes_[0])
+        # y_pred = np.full(shape=len(X),fill_value=self.classes_[0])
 
-        ########################## Manual method #################
         # def dist_euc(x,y):
         #     dist=0
         #     for j in range(len(x)):
         #           dist+=(x[j]-y[j])**2
         #     return(dist**0.5)
-        
         # for i in range (X.shape[0]) :
         #     distance=[]
         #     x=X[i,:]
@@ -55,24 +50,19 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         #                 if all(self.X_[e,:]==neib):
         #                     index=e
         #             y_pred[i]=self.y_[index]
-        
-        #################With pre-defined functions###########################
-        
+        # With pre-defined functions
         check_is_fitted(self)
         X = check_array(X)
-        y_pred = np.full(shape=len(X),fill_value=self.classes_[0])
-        dist=np.zeros((len(X),len(self.X_)))
+        y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
+        dist = np.zeros((len(X), len(self.X_)))
         for i in range(dist.shape[0]):
             for j in range(dist.shape[1]):
-                dist[i,j]=np.linalg.norm(X[i]-self.X_[j],2)
-        
-        neib=np.argmin(dist,axis=1)
-        y_pred = np.full(shape=len(X), dtype=self.classes_.dtype,fill_value=self.y_[neib])
-        
-        
+                dist[i, j] = np.linalg.norm(X[i]-self.X_[j], 2)
+        neib = np.argmin(dist, axis=1)
+        y_pred = np.full(shape=len(X), dtype=self.classes_.dtype,
+                         fill_value=self.y_[neib])
         # XXX fix
         return y_pred
-    
 
     def score(self, X, y):
         """Score calculation ."""
