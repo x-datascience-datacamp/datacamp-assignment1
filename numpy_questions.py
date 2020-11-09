f0b6f867da823e7742b4d5fd60f4fc8f5bc9ec9b
Louis <lmercier36@gmail.com>
2020-11-09 18:53:16 +0100

    Ajout Scikit Learn

--- numpy_questions.py
+++ numpy_questions.py
@@ -46,7 +46,7 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    if n_terms <= 0:
+    if n_terms == 0:
         res = 1
     else:
         L = [i for i in range(1, n_terms+1)]
--- sklearn_questions.py
+++ sklearn_questions.py
@@ -2,35 +2,65 @@
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
+from sklearn.utils.multiclass import check_classification_targets
 from sklearn.utils.validation import check_array
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
     """Write docstring
     """
+    
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """Write docstring
+        """To each point X
+        One can give a class Y
+        
+        Parameters : 
+        X : 2D array
+        y : 1D array, represents the classes
+        
+        Returns :
+        Nothing, it's a method that changes the attributes.
         """
+        
         X, y = check_X_y(X, y)
+        check_classification_targets(y)
         self.classes_ = np.unique(y)
         # XXX fix
+        self.X_ = X
+        self.y_ = y
         return self
 
     def predict(self, X):
-        """Write docstring
+        """One can predict what is the class of a given X
+        
+        Parameters :
+            X : 2D array
+        
+        Returns :
+            y : The class predicted
         """
+        
         check_is_fitted(self)
         X = check_array(X)
         y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
         # XXX fix
+        y_pred = np.array([self.y_[np.argmin(np.linalg.norm(x - self.X_, axis=1))] for x in X])
         return y_pred
 
     def score(self, X, y):
-        """Write docstring
+        """Gives the accuracy of model
+        
+        Parameters : 
+        X : a set of position, X_test
+        y : a set of class, y_test
+        
+        Returns :
+        Accuracy of model
         """
+        
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
         return np.mean(y_pred == y)
