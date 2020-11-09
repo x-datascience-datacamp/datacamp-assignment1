2ed9d7c82b48f71598a5f91e16900dfa8064bd01
ryanboustany <58556690+ryanboustany@users.noreply.github.com>
2020-11-09 21:37:09 +0100

    2

--- numpy_questions.py
+++ numpy_questions.py
@@ -3,6 +3,7 @@ import numpy as np
 
 
 def max_index(X):
+
     """Return the index of the maximum in a numpy array.
     Parameters
     ----------
@@ -30,6 +31,7 @@ def max_index(X):
 
 
 def wallis_product(n_terms):
+
     """Implement the Wallis product to compute an approximation of pi.
     See:
     https://en.wikipedia.org/wiki/Wallis_product
