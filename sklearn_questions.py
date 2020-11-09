46a2054e0bc1b51ab87ad170d2f7ad9dc358e6ea
Oscar Bouvier <oscar.bouvier@telecom-sudparis.eu>
2020-11-09 17:31:52 +0100

    formating

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -5,35 +5,37 @@ from sklearn.utils.validation import check_X_y, check_is_fitted
 from sklearn.utils.validation import check_array
 from sklearn.metrics import pairwise_distances
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    """One Nearest Neighbor model
+
+
+    """One Nearest Neighbor model.
     """
-    def __init__(self):  # noqa: D107
+    def __init__(self):  #noqa: D107
         pass
 
     def fit(self, X, y):
-        """Fit the model
+        """Fit the model.
 
         Parameters
         ----------
-        X : np array of training data
+        X : np array of training data.
 
-        y : np array of training labels
+        y : np array of training labels.
 
         Returns
         -------
-        self : fitted model
+        self : fitted model.
 
         Raises
         ------
         ValueError
-            If the number of class is too large (over 50)
+            If the number of class is too large (over 50).
 
         """
         X, y = check_X_y(X, y)
         self.classes_ = np.unique(y)
 
         if len(self.classes_) > 50:
-            raise ValueError("Unknown label type: number of classes too large")
+            raise ValueError("Unknown label type: number of class too large.")
 
         self.X_ = X
         self.y_ = y
@@ -41,15 +43,15 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         return self
 
     def predict(self, X):
-        """Compute the prediction of an new observation
+        """Compute the prediction of an new observation.
 
         Parameters
         ----------
-        X : np array of a new observation
+        X : np array of a new observation.
 
         Returns
         -------
-        y_pred : int predicted label
+        y_pred : int predicted label.
         """
         check_is_fitted(self)
         X = check_array(X)
@@ -60,16 +62,16 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         return y_pred
 
     def score(self, X, y):
-        """ Compute the accuracy of the model on set of observations and true labels
+        """ Compute the accuracy of the model on set of observations and true labels.
 
         Parameters
         ----------
-        X : np array of set of observations
-        y : np array of set of labels
+        X : np array of set of observations.
+        y : np array of set of labels.
 
         Returns
         -------
-        accuracy : float
+        accuracy : float.
         """
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
