59ca24831b62320a41360ee9bee26406d255fe36
unknown <taoufik.aghris@emines.um6p.ma>
2020-11-09 15:59:38 +0100

    Update

--- numpy_questions.py
+++ numpy_questions.py
@@ -24,20 +24,17 @@ def max_index(X):
         If the input is not a numpy error or
         if the shape is not 2D.
     """
+    i = 0
+    j = 0
 
+    # TODO
     if type(X) is not np.ndarray:
         raise ValueError("the input is not a numpy")
     if (len(X.shape) != 2):
         raise ValueError("the shape is not 2D")
-    R_max = []
-    C_max = []
-    for k in range(X.shape[0]):
-        R_max.append(np.max(X[k][:]))
-    X = X.T
-    for k in range(X.shape[0]):
-        C_max.append(np.max(X[k][:]))
-    i = R_max.index(np.max(R_max))
-    j = C_max.index(np.max(C_max))
+    ind = np.unravel_index(np.argmax(X), X.shape)
+    i = ind[0]
+    j = ind[1]
     return i, j
 
 
@@ -52,6 +49,12 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    count = lambda n: 4*n**2 / (4*n**2 - 1)
-    arr_n = np.array(range(1,n_terms+1))
-    return np.prod(count(arr_n))*2
+
+    if not isinstance(n_terms, int):
+        raise ValueError("n_terms should be an integer")
+    if n_terms < 0:
+        raise ValueError("n_terms should be at least 0")
+    pi_approx = 1.0
+    for n in range(1, n_terms+1):
+        pi_approx = pi_approx * 4*n**2/(4*n**2-1)
+    return 2*pi_approx
