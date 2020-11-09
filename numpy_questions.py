56915ab30128dad835bb5290eff6b48a4f5b3e93
Lokmen ELTARR <eltarr@ensta.fr>
2020-11-09 21:31:34 +0100

    aucune erreur

--- numpy_questions.py
+++ numpy_questions.py
@@ -27,35 +27,35 @@ def max_index(X):
     i = 0
     j = 0
     # TODO
-    if isinstance(X, (np.ndarray, np.generic))==False:
-    	raise ValueError #NameError("X is not an array\n") 
-    if X.ndim !=2:
-    	raise ValueError #NameError('Array shape is not 2D\n')
-    i,j = np.unravel_index(np.argmax(X, axis=None), X.shape)
+    if isinstance(X, (np.ndarray, np.generic)) is False:
+        raise ValueError
+    if X.ndim != 2:
+        raise ValueError
+    i, j = np.unravel_index(np.argmax(X, axis=None), X.shape)
     return i, j
 
 
 def wallis_product(n_terms):
-	"""Implement the Wallis product to compute an approximation of pi.
+    """Implement the Wallis product to compute an approximation of pi.
 
-	See:
-	https://en.wikipedia.org/wiki/Wallis_product
-	"""
-	# XXX : The n_terms is an int that corresponds to the number of
-	# terms in the product. For example 10000.
-	"""Return an approximation of mathematical constant pi using Wallis product
-	Parameters
-	----------
-	n_terms : Number of terms we use in the product. Must be a positive int
+    See:
+    https://en.wikipedia.org/wiki/Wallis_product
+    """
+    # XXX : The n_terms is an int that corresponds to the number of
+    # terms in the product. For example 10000.
+    """Return an approximation of mathematical constant pi using Wallis product
+    Parameters
+    ----------
+    n_terms : Number of terms we use in the product. Must be a positive int
 
-	Returns
-	-------
-	pi_approx : float
-				our approximation of the number pi
-	"""
-	i = 1
-	pi_approx = 1
-	while i <=n_terms:
-		pi_approx*=(4*i**2)/(4*i**2-1)
-		i+=1
-	return 2*pi_approx
+    Returns
+    -------
+    pi_approx : float
+                our approximation of the number pi
+    """
+    i = 1
+    pi_approx = 1
+    while i <= n_terms:
+        pi_approx *= (4*i**2)/(4*i**2-1)
+        i += 1
+    return 2*pi_approx
--- sklearn_questions.py
+++ sklearn_questions.py
@@ -7,34 +7,39 @@ from sklearn.utils.multiclass import check_classification_targets
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    """Write docstring
+    """We write our own 1-Nearest neighbor classifier
     """
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """Write docstring
+        """takes the training data as arguments,
+        which can be one array in the case of unsupervised learning,
+        or two arrays in the case of supervised learning
         """
         X, y = check_X_y(X, y)
         check_classification_targets(y)
         self.classes_ = np.unique(y)
         self.X_ = X
         self.y_ = y
+        self.n_features_in_ = X.shape[1]
         return self
 
     def predict(self, X):
-        """Write docstring
+        """does the next classification on the data X
         """
         check_is_fitted(self)
         X = check_array(X)
-        y_pred = np.full(shape=len(X), fill_value=self.classes_[0], dtype = self.classes_.dtype)
-        for i,x in enumerate(X):
+        fill = self.classes_[0]
+        dtype = self.classes_.dtype
+        y_pred = np.full(shape=len(X), fill_value=fill, dtype=dtype)
+        for i, x in enumerate(X):
             closest = np.argmin(np.linalg.norm(self.X_-x, axis=1))
             y_pred[i] = self.y_[closest]
         return y_pred
 
     def score(self, X, y):
-        """Write docstring
+        """compute performance of the method
         """
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
