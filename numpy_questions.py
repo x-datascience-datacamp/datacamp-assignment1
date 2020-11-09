b83c5e2551cc571f7e505b09d18f314fee075b4b
AbdelMouaiz <tebjou.abdelmouaiz@gmail.com>
2020-11-09 15:49:27 +0100

    premier commit

--- numpy_questions.py
+++ numpy_questions.py
@@ -27,6 +27,13 @@ def max_index(X):
     i = 0
     j = 0
 
+    if type(X)!= numpy.ndarray :
+        raise ValueError("the input should be an ndarray ")
+    if X.ndim !=2:
+        raise ValueError('the shape of the input is not 2D')
+    a= X.index(max(X))
+    i=a[0]
+    j=a[1]
     # TODO
 
     return i, j
@@ -43,4 +50,8 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    a=1
+    for i in range(n_terms):
+        a=a*((2*n/(2*n-1))*(2*n/(2*n+1)))
+
+    return a
