9edf410971a893c3acb0098d76cbb32703dbdecf
louis-oscarmorel <louis-o.morel@hotmail.fr>
2020-11-09 14:48:55 +0100

    test1ok

--- numpy_questions.py
+++ numpy_questions.py
@@ -28,9 +28,15 @@ def max_index(X):
     j = 0
 
     # TODO
-
+    try:
+        assert type(X) == type(np.array(1))
+        assert X.ndim == 2
+    except AssertionError:
+        raise ValueError
+    max = np.unravel_index(np.argmax(X, axis=None), X.shape)
+    i = max[0]
+    j = max[1]
     return i, j
-
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
@@ -43,4 +49,8 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    pi = 1
+    for i in range(1, n_terms+1):
+        pi *= 4*(i**2)/(4*(i**2)-1)
+    pi *= 2
+    return pi
