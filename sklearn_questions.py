afdfeb881369053350f305595fc58317180272a3
ChristosKts <christos.katsoulakis@ensae.fr>
2020-11-09 18:47:13 +0100

    Questions solved

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -7,14 +7,14 @@ from sklearn.utils.multiclass import check_classification_targets
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    """Write docstring
-    """
+    """Define class for the model One Nearest Neighbor."""
+
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """Write docstring
-        """
+        """Fit the model."""
+
         X, y = check_X_y(X, y)
         check_classification_targets(y)
         self.classes_ = np.unique(y)
@@ -23,11 +23,11 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         return self
 
     def predict(self, X):
-        """Write docstring
-        """
+        """Return the predictions of the model."""
+
         check_is_fitted(self)
         X = check_array(X)
-        y_pred = np.full(shape=len(X), 
+        y_pred = np.full(shape=len(X),
                          fill_value=self.classes_[0],
                          dtype=self.classes_.dtype)
         for i, x in enumerate(X):
@@ -36,8 +36,8 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         return y_pred
 
     def score(self, X, y):
-        """Write docstring
-        """
+        """Return the score of the model."""
+
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
         return np.mean(y_pred == y)
