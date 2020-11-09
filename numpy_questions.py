9167ff74cab46441345f9bf49a0e9600cab1e2b1
Emma Sarfati <emmasarfati@MacBook-Pro-de-Emma.local>
2020-11-09 14:52:31 +0100

    work

--- numpy_questions.py
+++ numpy_questions.py
@@ -53,9 +53,9 @@ def wallis_product(n_terms):
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
 
-    a = [(4 * n**2) / (4 * n**2 - 1) for n in range(1,n_terms)]
-
-    if len(a)
+    if n_terms == 0:
+        return 2
+    else:
+        a = [(4 * n**2) / (4 * n**2 - 1) for n in range(1,n_terms+1)]
 
     return 2*reduce(lambda x,y: x*y, a)
-
