906ad4a148185f0362cd16bd5e1610d07648c38d
louis-oscarmorel <louis-o.morel@hotmail.fr>
2020-11-09 17:45:07 +0100

    flake1

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -8,15 +8,13 @@ from sklearn.utils.multiclass import check_classification_targets
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    """  One Nearest Neighbor Algorithm   
-         Takes the estimator and the classifier as inputs 
-         Returns fit, predict and score methods """
+    """One Nearest Neighbor Algorithm."""
     
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """ Fit the model to X and y 
+        """Fit the model to X and y. 
         
         Parameters
         ----------
@@ -36,7 +34,6 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         ------
         ValueError
             If there are more than 50 classes.
-        
         """
         
         X, y = check_X_y(X, y)
@@ -48,19 +45,19 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         return self
 
     def predict(self, X):
-        """ Predicts y for a given X 
+        """Predicts y for a given X. 
         
         
         Parameters
         ----------
         X : ndarray of shape (n_samples, n_features)
-            test data
+            test data.
             
         Returns
         -------
         y_pred : prediction from X
-        
         """
+        
         check_is_fitted(self)
         X = check_array(X)
         y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
@@ -70,7 +67,7 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
 
 
     def score(self, X, y):
-        """ Compare y_pred and real y to ive the score 
+        """ Compare y_pred and real y to ive the score. 
         
         Parameters
         ----------
@@ -82,8 +79,8 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         Returns
         -------
         score : float
-        
         """
+        
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
         return np.mean(y_pred == y)
