bfdc85e92e43aeba144bbfc321f3b5b585852462
Nathan Xerri <nathan.xerri@telecom-paristech.fr>
2020-11-09 12:24:27 +0100

    solution to numpy_questions1

--- numpy_questions.py
+++ numpy_questions.py
@@ -24,11 +24,16 @@ def max_index(X):
         If the input is not a numpy error or
         if the shape is not 2D.
     """
+    if np.shape(X) != 2 :
+        raise ValueError("Shape of X must be 2")
+
+
     i = 0
     j = 0
 
+    i = np.argmax(X, axis=0)
+    j = np.argmax(X[i,:])
     # TODO
-
     return i, j
 
 
@@ -43,4 +48,8 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    a = 1
+    for k in range(n_terms):
+        a = a*((4*k**2)/(4*k**2 -1))
+
+    return a
