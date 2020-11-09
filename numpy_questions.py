cbe06ae672225aadf1a8d22bd076463fe0827bfa
unknown <taoufik.aghris@emines.um6p.ma>
2020-11-09 14:04:40 +0100

    test

--- numpy_questions.py
+++ numpy_questions.py
@@ -28,7 +28,19 @@ def max_index(X):
     j = 0
 
     # TODO
-
+    if type(X) is not np.ndarray:
+        raise ValueError("the input is not a numpy")
+    if (len(X.shape)!=2):
+         raise ValueError("the shape is not 2D")
+    R_max=[]
+    C_max=[]
+    for k in range(X.shape[0]) :
+        R_max.append(np.max(X[k][:]))
+    X=X.T
+    for k in range(X.shape[0]) :
+        C_max.append(np.max(X[k][:]))
+    i=R_max.index(np.max(R_max))
+    j=C_max.index(np.max(C_max))
     return i, j
 
 
@@ -43,4 +55,19 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    if n_terms<1:
+        return 2.
+    pi = 0.0   
+    for i in range(1, n_terms+1):
+        print(i)
+        x = 4 * (i ** 2)
+        y = x - 1
+        z = float(x) / float(y)
+        if (i == 1):
+            pi = z
+        else:
+            pi *= z
+    pi *= 2
+    return pi
+
+print(wallis_product(0))
