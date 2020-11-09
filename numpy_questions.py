db587745d07295f05efd65049d7ac748656aaa31
BERRADI Aymane <aymane.berradi@emines.um6p.ma>
2020-11-09 12:52:03 +0100

    done

--- numpy_questions.py
+++ numpy_questions.py
@@ -26,9 +26,22 @@ def max_index(X):
     """
     i = 0
     j = 0
-
+    if type(X) is not np.ndarray :
+        raise ValueError('the input is not a numpy')
+  
     # TODO
-
+    if (len(X.shape)>2):
+         raise ValueError('the shape is not 2D')
+    rows_max=[]
+    cols_max=[]
+    for r in range(X.shape[0]):
+        rows_max.append(np.max(X[r][:]))
+    X=X.T
+    for c in range(X.shape[0]):
+        cols_max.append(np.max(X[c][:]))
+    i=rows_max.index(np.max(rows_max))
+    j=cols_max.index(np.max(cols_max))
+    
     return i, j
 
 
@@ -41,6 +54,16 @@ def wallis_product(n_terms):
     XXX : write Parameters and Returns sections as above.
 
     """
+    pi_approx = 0.0   
+    for i in range(1, n_terms+1):
+        a = 4 * (i ** 2)
+        b = a - 1
+        z = float(a) / float(b)
+        if (i == 1):
+            pi_approx = z
+        else:
+            pi_approx *= z
+    pi_approx *= 2
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    return pi_approx
