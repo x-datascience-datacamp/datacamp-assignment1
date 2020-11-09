bc5bd0d4f88caf9df2928ded054e556eb9cf3406
BERRADI Aymane <aymane.berradi@emines.um6p.ma>
2020-11-09 17:18:59 +0100

    'flakee'

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -1,7 +1,7 @@
 # noqa: D100
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
-from sklearn.utils.validation import check_X_y, check_is_fitted
+from sklearn.utils.validation import check_X_y
 from sklearn.utils.validation import check_array
 from sklearn.metrics.pairwise import pairwise_distances
 
@@ -13,8 +13,7 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         pass
 
     def fit(self, X, y):
-        """Fitting function.
-        """
+        "Fitting function."
         X, y = check_X_y(X, y)
         self.classes_ = np.unique(y)
         if len(self.classes_) > 30:
@@ -27,7 +26,8 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
     def predict(self, X):
         """Predit function.
         """
-        check_is_fitted(self)
+        if not hasattr(self, "X_"):
+            raise ValueError("")
         X = check_array(X)
         y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
         all_distances = pairwise_distances(X, self.X_)
