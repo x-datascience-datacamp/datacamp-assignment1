07f954fc03e14cbd6f7e40221c537dca0ef05c86
MathieuJuttet <mathieujuttet@gmail.com>
2020-11-09 19:00:22 +0100

    my_branch

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -3,74 +3,66 @@ import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
 from sklearn.utils.validation import check_array
+from sklearn.utils.multiclass import type_of_target
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    """
-    Parameters
-    ----------
-    dqfqsd
-    Returns
-    -------
-    wdfs
-    Raises
-    ------
-    sdfsd
-    """
+    """Estimator to find the 1-nearest neighbor with SKlean."""
+
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """
+        """Write docstring.
+
         Parameters
         ----------
-        dxwfsdf
+        X : inputs
+        y : labels
+
         Returns
         -------
-        wsdqs
-        Raises
-        ------
-        qsdqsd
+        Fitted model
         """
         X, y = check_X_y(X, y)
+        if type_of_target(y) not in ['binary', 'multiclass', 'unknown']:
+            raise ValueError(f"Unknown label type: {y}")
         self.classes_ = np.unique(y)
         # XXX fix
         self.X_ = X.copy()
         self.y_ = y.copy()
-
         return self
 
     def predict(self, X):
-        """
+        """Write docstring.
+
         Parameters
         ----------
-        dxwfsdf
+        X : inputs
         Returns
         -------
-        wsdqs
-        Raises
-        ------
-        qsdqsd
+        y_pred = 1-nearest neighbor
         """
         check_is_fitted(self)
         X = check_array(X)
         y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
         # XXX fix
-        distances = np.apply_along_axis(((self.X_ - sum(axis=1))**2), 1, X)
+        def compute_distance(v): return ((self.X_ - v)**2).sum(axis=1)
+        distances = np.apply_along_axis(compute_distance, 1, X)
         y_pred = self.y_[distances.argmin(axis=1)]
         return y_pred
 
     def score(self, X, y):
-        """
+        """Write docstring.
+
         Parameters
         ----------
-        dxwfsdf
+        X : inputs
+        y : labels
+
         Returns
         -------
-        wsdqs
-        Raises
-        ------
-        qsdqsd
+        Mean of the good prediction
         """
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
