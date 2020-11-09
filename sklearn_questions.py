f14779abebc90a6fcbb6699ebf76253939e1d208
maximeberillon <maxime.berillon@icloud.com>
2020-11-09 15:25:43 +0000

    Second part

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
 from sklearn.utils.validation import check_array
+from scipy.spatial import distance
+from sklearn.utils.multiclass import check_classification_targets
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
@@ -15,8 +17,10 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         """Write docstring
         """
         X, y = check_X_y(X, y)
+        check_classification_targets(y)
         self.classes_ = np.unique(y)
-        # XXX fix
+        self.X_ = X
+        self.y_ = y
         return self
 
     def predict(self, X):
@@ -24,8 +28,11 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         """
         check_is_fitted(self)
         X = check_array(X)
-        y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
-        # XXX fix
+        y_pred = np.full(shape=len(X), fill_value=self.classes_[0],
+                         dtype=self.classes_.dtype)
+        for idx, x in enumerate(X):
+            d = np.array([distance.minkowski(x, k, 2) for k in self.X_])
+            y_pred[idx] = self.y_[d.argmin()]
         return y_pred
 
     def score(self, X, y):
