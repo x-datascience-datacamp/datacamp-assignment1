91cb77fbb32189d99c0958d98fafc45a070b6ebd
Youssef Brachmi <youssefbrachmi@gmail.com>
2020-11-09 17:02:39 +0100

    Solving both questions and correct flake8

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
 from sklearn.utils.validation import check_array
+from sklearn.utils.multiclass import check_classification_targets
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
@@ -12,24 +13,32 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         pass
 
     def fit(self, X, y):
-        """Write docstring
+        """Fit the data to store in variables
         """
         X, y = check_X_y(X, y)
+        check_classification_targets(y)
+
         self.classes_ = np.unique(y)
-        
+        self.X_ = X
+        self.y_ = y
         return self
 
     def predict(self, X):
-        """Write docstring
+        """Predict the nearest neighbor for each element based
+        on euclidian distance
         """
         check_is_fitted(self)
         X = check_array(X)
-        y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
-        # XXX fix
+        y_pred = np.full(shape=len(X), fill_value=self.classes_[0],
+                         dtype=self.classes_.dtype)
+        for i in range(len(X)):
+            distances_eucli = np.linalg.norm(X[i] - self.X_, axis=1)
+            nearest = np.argmin(distances_eucli)
+            y_pred[i] = self.y_[nearest]
         return y_pred
 
     def score(self, X, y):
-        """Write docstring
+        """Scoring the correct nearest neighbors predicted
         """
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
