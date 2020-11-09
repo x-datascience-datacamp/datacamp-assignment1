a7b3c2e8dca8b160dbe857d58cd3bcabd9a24c80
Ophélie Bleu <opheliebleu@MacBook-Air-de-Ophelie.local>
2020-11-09 15:49:37 +0100

    correct some mistakes

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -16,30 +16,30 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         pass
 
     def fit(self, X, y):
-        """Fit X to y."""
+        """Prend en entrée X et y et les garde en mémoire."""
         X, y = check_X_y(X, y)
         self.classes_ = np.unique(y)
         check_classification_targets(self.classes_)
         # XXX fix
-        self.sample_ = X
+        self.sampleX_ = X
         self.y_ = y
         return self
 
     def predict(self, X):
-        """Predict y with X."""
+        """Prend X en entrée et prédit y."""
         check_is_fitted(self, ["classes_"])
         X = check_array(X)
         y_pred = np.full(shape=len(X), fill_value=self.classes_[0],
                          dtype=self.classes_.dtype)
         # XXX fix
         for i in range(0, len(y_pred)):
-            distances = np.sqrt(np.sum(np.square(X[i] - self.sample_),
+            distances = np.sqrt(np.sum(np.square(X[i] - self.sampleX_),
                                 axis=-1, keepdims=True))
             y_pred[i] = self.y_[np.argmin(distances)]
         return y_pred
 
     def score(self, X, y):
-        """Score y predicted with real y."""
+        """Score y predit par rapport au vrai y."""
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
         return np.mean(y_pred == y)
