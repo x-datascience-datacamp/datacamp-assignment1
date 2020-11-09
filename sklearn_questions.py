67e93921e748bf261f7fcd3d864308525aa8af32
Théo Dullin <theo.dullin@ensta-paris.fr>
2020-11-10 00:48:46 +0100

    flake8 changes

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -61,7 +61,7 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         check_is_fitted(self)
         X = check_array(X)
         y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
-        y_pred = np.array([self.y_[np.argmin(np.linalg.norm(x - self.X_, axis=1))]for x in X])
+        y_pred = np.array([self.y_[ np.argmin(np.linalg.norm(xl - self.X_, axis=1)) ] for xl in X ])
         return y_pred
 
     def score(self, X, y):
