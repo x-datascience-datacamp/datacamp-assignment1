d278ba06288f574fd68a24769baa603d4e41cbf9
almarouk <abdelrahman.almarouk@gmail.com>
2020-11-09 12:36:07 +0100

    Update numpy_questions.py

--- numpy_questions.py
+++ numpy_questions.py
@@ -28,6 +28,13 @@ def max_index(X):
     j = 0
 
     # TODO
+    if not isinstance(X, np.ndarray):
+        raise ValueError("X should be a numpy array")
+    if len(X.shape) != 2:
+        raise ValueError("X should be a 2D array")
+    ind = np.unravel_index(np.argmax(X), X.shape)
+    i = ind[0]
+    j = ind[1]
 
     return i, j
 
@@ -38,9 +45,32 @@ def wallis_product(n_terms):
     See:
     https://en.wikipedia.org/wiki/Wallis_product
 
-    XXX : write Parameters and Returns sections as above.
+    Parameters
+    ----------
+    n_terms : int
+        The number of terms in the product. For example 10000.
+
+    Returns
+    -------
+    estimated_pi : float
+        The estimated value of pi using Wallis product
+
+    Raises
+    ------
+    ValueError
+        If the input is not an integer or
+        if it's less than 0.
 
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    if not isinstance(n_terms, int):
+        raise ValueError("n_terms should be an integer")
+    if n_terms < 0:
+        raise ValueError("n_terms should be at least 0")
+
+    estimated_pi = 1.0
+    for n in range(1, n_terms+1):
+        estimated_pi = estimated_pi * 4*n**2/(4*n**2-1)
+
+    return 2*estimated_pi
