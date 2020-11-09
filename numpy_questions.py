7c11051c9af2f4c58aaddf6b6ca638491a6807b6
Bastien B <bastien.billiot@laposte.net>
2020-11-09 16:51:21 +0100

    Update n°2 Assignment 1

--- numpy_questions.py
+++ numpy_questions.py
@@ -1,7 +1,7 @@
 # noqa: D100
 import numpy as np
-import functools
 from functools import reduce
+
 
 def max_index(X):
     """Return the index of the maximum in a numpy array.
@@ -30,12 +30,14 @@ def max_index(X):
 
     # TODO
     if type(X) is not np.ndarray:
-        raise ValueError('The input is not a numpy array') 
-    if len(X.shape)!=2: 
+        raise ValueError('The input is not a numpy array')
+    if len(X.shape) != 2:
         raise ValueError('The shape is not 2D')
-    (i,j) = np.unravel_index(np.argmax(X, axis=None), X.shape)
+    (i, j) = np.unravel_index(np.argmax(X, axis=None), X.shape)
 
     return i, j
+
+
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
 
@@ -45,8 +47,9 @@ def wallis_product(n_terms):
     XXX : write Parameters and Returns sections as above.
 
     """
-    if n_terms == 0 :
+    if n_terms == 0:
         pi = 2
     else:
-        pi = 2 * reduce(lambda x, y: x*y, [(4.0*(i**2))/(4.0*(i**2)-1) for i in range (1, n_terms+1)])
+        d = [(4.0*(i**2))/(4.0*(i**2)-1) for i in range(1, n_terms+1)]
+        pi = 2 * reduce(lambda x, y: x*y, d)
     return pi
--- sklearn_questions.py
+++ sklearn_questions.py
@@ -1,11 +1,11 @@
 # noqa: D100
-#%%
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
 from sklearn.utils.validation import check_array
 from sklearn.utils.multiclass import check_classification_targets
 from scipy.spatial import distance_matrix
+
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
     """Define a1-nearest neighbor classifier
@@ -18,45 +18,46 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         X: observations of the training set (numpy array)
         y: labels of the training set (numpy array)
         ----------------------
-        Parameters: 
+        Parameters
         X: ndarray (nb_samples, nb_features)
         y : ndarray (nb_samples,)
         """
         X, y = check_X_y(X, y)
         check_classification_targets(y)
         self.classes_ = np.unique(y)
-        self.X_train_= X
-        self.y_train_= y
+        self.X_train_ = X
+        self.y_train_ = y
         return self
 
     def predict(self, X):
-        """Predict the class label for new data points 
+        """Predict the class label for new datapoints
         ----------------------
-        Parameters: 
-        X: Data Samples we want to get the label of - ndarray (nb_data_points, nb_features)
+        Parameters:
+        X: Data Samples we want to get the label of
+        ndarray (nb_data_points, nb_features)
         ----------------------
         Returns
         y:label (class) for each data sample - ndarray (nb_labels,)
-        
         """
-        check_is_fitted(self, ['X_train_','y_train_','classes_'])
+        check_is_fitted(self, ['X_train_', 'y_train_', 'classes_'])
         X = check_array(X)
         y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
-        y_pred = self.y_train_[np.argmin(distance_matrix(X, self.X_train_), axis=1)]
+        dist_ = np.argmin(distance_matrix(X, self.X_train_), axis=1)
+        y_pred = self.y_train_[dist_]
         return y_pred
 
     def score(self, X, y):
         """Compute the accuracy for a test sample
-         ----------------------
-        Parameters: 
+        ----------------------
+        Parameters:
         X : samples - ndarray (nb_samples, nb_features)
         y : true class for X - ndarray (nb_samples,)
         ----------------------
         Returns
-        float corresponding to the mean accuracy of the class predicted for data X compared to value y
+        float corresponding to the mean accuracy of the class predicted for
+        data X compared to value y
         """
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
         return np.mean(y_pred == y)
-
-# %%
+      
