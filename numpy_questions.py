24cad1269d5c702dda3676ac1f539f62de3f2140
unknown <arab.nawel@outlook.com>
2020-11-09 12:40:17 +0100

    First assignment Please enterthe commit message for your changes. Lines starting

--- numpy_questions.py
+++ numpy_questions.py
@@ -26,8 +26,8 @@ def max_index(X):
     """
     i = 0
     j = 0
-
     # TODO
+    i,j = np.unravel_index(np.argmax(X,axis=None),X.shape)
 
     return i, j
 
@@ -43,4 +43,11 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+
+    halfpi = 1.0
+
+    for i in range(1, n_terms):
+        numer = i * 2
+        denom = i * 2 - 1
+        halfpi = halfpi * (numer * numer) / (denom * (denom + 2))
+    return (2 * halfpi)
