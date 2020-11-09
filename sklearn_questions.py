8e9707036825de7b4bd1df825f73a8f2ea89f151
AlexTaffet <alexandre.taffet@telecom-sudparis.eu>
2020-11-09 19:19:19 +0100

    OneNearestNeigbor first version

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -6,31 +6,36 @@ from sklearn.utils.validation import check_array
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    """Write docstring
-    """
+    """ Create the 1-N-N Classifier"""
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """Write docstring
-        """
+        """Store trainning data"""
         X, y = check_X_y(X, y)
         self.classes_ = np.unique(y)
         # XXX fix
+        self.Xtrain_ = X
+        self.ytrain_ = y
         return self
 
     def predict(self, X):
-        """Write docstring
-        """
+        """Predict the class of elements in the testing dataset X"""
         check_is_fitted(self)
         X = check_array(X)
-        y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
+        y_pred = np.full(shape=len(X), fill_value=self.classes_[0],
+                         dtype=self.classes_.dtype)
         # XXX fix
+        for i in range(len(X)):
+            L = []
+            for k in range(len(self.Xtrain_)):
+                L.append(np.linalg.norm(X[i]-self.Xtrain_[k]))
+            y_pred[i] = self.ytrain_[np.argmin(L)]
         return y_pred
 
     def score(self, X, y):
-        """Write docstring
-        """
+        """Compare the calculated prediction of X y_pred
+        and the real value of the prediction y"""
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
         return np.mean(y_pred == y)
