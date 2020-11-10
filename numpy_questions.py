92b4389859856235f42e4d0bb471add27c6fc8b1
Eloi Alardet <eloi.alardet@gmail.com>
2020-11-10 12:33:35 +0100

    skearn questions
    Solving sklearn questions + solving tests

--- numpy_questions.py
+++ numpy_questions.py
@@ -12,10 +12,10 @@ def max_index(X):
 
     Returns
     -------
-    i : int
+    i : integer
         The row index of the maximum.
 
-    j : int
+    j : integer
         The column index of the maximum.
 
     Raises
@@ -24,10 +24,10 @@ def max_index(X):
         If the input is not a numpy error or
         if the shape is not 2D.
     """
-    if not isinstance(X, np.ndarray):
+    if (type(X) != np.ndarray):
         raise ValueError("X is an numpy array")
-    if X.ndim != 2:
-        raise ValueError("X must be 2D")
+    if (X.ndim != 2):
+        raise ValueError("X is in 2-dimensions")
     i, j = np.unravel_index(np.argmax(X, axis=None), X.shape)
     return i, j
 
@@ -57,7 +57,8 @@ def wallis_product(n_terms):
     # terms in the product. For example 10000.
     if n_terms < 0:
         raise ValueError("Number of terms must be higher than 0")
-    pi = 1.
+    approx = 1.
     for i in range(1, n_terms+1):
-        pi *= (4*(i**2)) / ((4*(i**2)-1))
-    return pi*2
+        approx *= (4*(i**2)) / (4*(i**2)-1)
+    pi = approx*2
+    return pi
--- sklearn_questions.py
+++ sklearn_questions.py
@@ -1,56 +1,71 @@
 # noqa: D100
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
-from sklearn.utils.validation import check_X_y, check_is_fitted
-from sklearn.utils.validation import check_array
+from sklearn.utils.validation import check_X_y, check_is_fitted, check_array
+from sklearn.utils.multiclass import type_of_target
+from sklearn.metrics.pairwise import euclidean_distances
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    """Write docstring
-    """
+    """Model for getting the nearest neighbor with sklearn."""
+
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """Write docstring
-        entrainer le model: predire la classe du point le plus
+        """Train the model.
+
         Parameters
         ----------
-        X : ndarray of shape
-            The input array.
-        y : array
+        X : np array
+            Input array.
+        y : np array
             Labels array
-    
         Returns
         -------
-        i : int
-            The row index of the maximum.
-    
-        j : int
-            The column index of the maximum.
-    
-        Raises
-        ------
-        ValueError
-            If the input is not a numpy error or
-            if the shape is not 2D.
+        self : model
         """
         X, y = check_X_y(X, y)
+        self.n_features_in_ = X.shape[1]
         self.classes_ = np.unique(y)
-        # XXX fix
+        if type_of_target(y) not in ['binary', 'multiclass', 'unknown']:
+            raise ValueError(f"Unknown label type: {y}")
+        self.X_ = X
+        self.y_ = y
         return self
 
     def predict(self, X):
-        """Write docstring
+        """Predict outputs.
+
+        Parameters
+        ----------
+        X : np array
+            Input array
+        Returns
+        -------
+        y_pred : predictions of the model
         """
         check_is_fitted(self)
         X = check_array(X)
-        y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
-        # XXX fix
+        y_pred = np.full(shape=len(X), fill_value=self.classes_[0],
+                         dtype=self.classes_.dtype)
+        arg = np.argmin(euclidean_distances(X, self.X_), axis=1)
+        y_pred = self.y_[arg]
         return y_pred
 
     def score(self, X, y):
-        """Write docstring
+        """Return the score of our model.
+
+        Parameters
+        ----------
+        X : np array
+            Input array.
+        y : np array
+            Labels array
+        Returns
+        -------
+        score : float
+                accuracy of our model.
         """
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
