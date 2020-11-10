4b667cdbb79dc148a6bed5b1cff5b9c985e71c51
ryanboustany <58556690+ryanboustany@users.noreply.github.com>
2020-11-10 12:01:44 +0100

    FINAL

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -3,89 +3,76 @@ import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
 from sklearn.utils.validation import check_array
-from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.utils.multiclass import type_of_target
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    """One Nearest Neighbor Classifier  - Scikit Learn API."""
+    """OneNearestNeighbor Classifier."""
 
     def __init__(self):  # noqa: D107
         pass
 
-    def nearest_neighbor_index(self, data):
-        """
-        Find nearest datapoint in the predictors.
-
-        Parameters
-        ----------
-        self : OneNearestNeihbor() Model
-        data : ndarray of shape (n_features) - a sample of data
-        Returns
-        -------
-        index : Index of the nearest data point
-
-        """
-        dist = euclidean_distances(self.X_, [data])
-        return np.argmin(dist)
-
     def fit(self, X, y):
-        """
-        Fit function of our One nearest neighbor model.
+        """Fit the OneNearestNeighbor Classifier.
 
         Parameters
         ----------
-        X : ndarray of training predictors (n_samples, n_features)
-        y : ndarray of training target values (n_samples)
+        X : ndarray of shape (n_samples, n_features)
+            The input array.
+        y : ndarray of shape (n_samples,)
+            The labels.
         Returns
         -------
-        self : OneNearestNeighbor() class
-        Raises
-        ------
-        ValueError
-            Maximum number of classes has been reached
+        self : object
+            Returns self.
 
         """
         X, y = check_X_y(X, y)
+        if type_of_target(y) not in ['binary', 'multiclass', 'unknown']:
+            raise ValueError(f"Unknown label type: {y}")
         self.classes_ = np.unique(y)
-        print(self.classes_)
-        if len(self.classes_) > 50:
-            raise ValueError("Unknown label type: ")
-        self.X_ = X
-        self.y_ = y
-
+        self.X_fit_ = X.copy()
+        self.y_fit_ = y.copy()
         return self
 
     def predict(self, X):
-        """
-        Predict function of our One nearest neighbor model.
+        """Predicting function.
 
         Parameters
         ----------
-        X : ndarray of test predictors (n_samples, n_features)
+        X : ndarray of shape (n_samples, n_features)
+            The input array.
         Returns
         -------
-        y_pred = ndarray the predicted values (n_samples)
+        y : ndarray of shape (n_samples, 1)
+            The predicted labels.
 
         """
         check_is_fitted(self)
         X = check_array(X)
-        y_pred = np.full(shape=len(X), fill_value=self.classes_[0], dtype=self.classes_.dtype)
-        for i in range(0, len(X)):
-            y_pred[i] = self.y_[self.nearest_neighbor_index(X[i])]
-
+        y_pred = np.full(shape=len(X), fill_value=self.classes_[0],
+                         dtype=self.classes_.dtype)
+        for i, test_pt in enumerate(X):
+            distances = np.array([np.linalg.norm(test_pt - fit_pt, 2)
+                                  for fit_pt in self.X_fit_])
+            nn = np.argmin(distances)
+            y_pred[i] = self.y_fit_[nn]
         return y_pred
 
     def score(self, X, y):
         """
-        Score function of our One nearest neighbor model.
+        Mean correct score of the classifier.
 
         Parameters
         ----------
-        X : ndarray of test predictors (n_samples, n_features)
-        y : ndarray of test target values (n_samples)
+        X : ndarray of shape (n_samples, n_features)
+            The input array.
+        y : ndarray of shape (n_samples,)
+            The labels.
         Returns
         -------
-        The accuracy of the predictions using a mean function.
+        self : object
+            Returns self.
 
         """
         X, y = check_X_y(X, y)
