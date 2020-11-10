1bd6245bf48eb7f2f95cd9ff1a0a87edae9f2792
romainilbert <74177821+romainilbert@users.noreply.github.com>
2020-11-10 10:29:41 +0100

    up

---
+++ numpy_questions.py
@@ -0,0 +1,16 @@
+# noqa: D100
+import numpy as np
+def max_index(X):
+    i = 0
+    j = 0
+    if type(X) is not np.ndarray :
+        raise ValueError('The input is not a np array')
+    if len(X.shape) !=2 : 
+        raise ValueError('X is not a matrix')
+    (i, j) = np.unravel_index(np.argmax(X), X.shape) 
+    return (i, j)
+def wallis_product(n_terms):
+    X = np.arange(1, n_terms+1, dtype = float)
+    X = 4 * X**2 / (4 * X**2 - 1)
+    pi = 2 * np.product(X)
+    return pi
