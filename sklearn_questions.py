55fc73186cfbb9b62aa3edcc4bf70005600ae91f
ryanboustany <58556690+ryanboustany@users.noreply.github.com>
2020-11-10 01:07:06 +0100

    11

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -1,36 +1,48 @@
-# noqa: D1000
+# noqa: D100
+
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
 from sklearn.utils.validation import check_array
+from sklearn.utils.multiclass import check_classification_targets
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    """Write docstring
-    """
+    """Write docstring."""
+
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """Write docstring
-        """
+
+        """Write docstring."""
+
         X, y = check_X_y(X, y)
+        check_classification_targets(y)
         self.classes_ = np.unique(y)
-        # XXX fix
+        self.X_ = X
+        self.y_ = y
         return self
 
     def predict(self, X):
-        """Write docstring
-        """
+
+        """Write docstring."""
+
         check_is_fitted(self)
         X = check_array(X)
-        y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
-        # XXX fix
+        y_pred = np.full(shape=len(X), fill_value=self.classes_[0],
+                         dtype=self.classes_.dtype)
+        for j in range(len(X)):
+            for i in range(len(self.X_):
+                dist = np.array([np.linalg.norm(self.X_[i] - X[j])])
+                nn_ind = np.argmin(dist)
+                y_pred[j] = self.y_[nn_ind]
         return y_pred
 
     def score(self, X, y):
-        """Write docstring
-        """
+
+        """Write docstring."""
+
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
         return np.mean(y_pred == y)
