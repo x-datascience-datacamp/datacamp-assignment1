b28c94227fbf1f5a23738f1f561ea2512f582211
Marko Avila <mavila@telecom-paristech.fr>
2020-11-10 10:24:41 +0100

    updf

--- numpy_questions.py
+++ numpy_questions.py
@@ -4,16 +4,20 @@ import numpy as np
 
 def max_index(X):
     """Return the index of the maximum in a numpy array.
+
     Parameters
     ----------
     X : ndarray of shape (n_samples, n_features)
         The input array.
+
     Returns
     -------
     i : int
         The row index of the maximum.
+
     j : int
         The column index of the maximum.
+
     Raises
     ------
     ValueError
@@ -22,26 +26,27 @@ def max_index(X):
     """
     i = 0
     j = 0
-
-    #TODO
-    if type(X) != np.ndarray or len(X.shape) != 2:
-        raise ValueError("Value Error")
-    else:
-        d=np.where(X==np.max(X, axis=None))
-        i=d[0][0]
-        j=d[1][0]
-
+    if type(X) != np.ndarray:
+        raise ValueError("X must be a np.array")
+    if len(X.shape) != 2:
+        raise ValueError("Input shape must be 2D")
+    d=np.where(X==np.max(X,axis=None))
+    i=d[0][0]
+    j=d[1][0]
     return i, j
 
 
 def wallis_product(n_terms):
-     """Implement the Wallis product to compute an approximation of pi.
+    """Implement the Wallis product to compute an approximation of pi.
     See:
     https://en.wikipedia.org/wiki/Wallis_product
-    XXX : write Parameters and Returns sections as above.
+    Parameters
+    ----------
+    n_terms : number for terms in the product. Higher is more precise.
+    Returns
+    -------
+    res : approximation of pi
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
     res=2
     if n_terms>0:
         for i in range(1, n_terms+1):
