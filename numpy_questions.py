3445fdb8ac4a3cdcb29067124a3dc8d8c3350623
Marko Avila <mavila@telecom-paristech.fr>
2020-11-10 10:39:57 +0100

    upd

--- numpy_questions.py
+++ numpy_questions.py
@@ -4,20 +4,62 @@ import numpy as np
 
 def max_index(X):
     """Return the index of the maximum in a numpy array.
-
     Parameters
     ----------
     X : ndarray of shape (n_samples, n_features)
         The input array.
-
     Returns
     -------
     i : int
         The row index of the maximum.
-
     j : int
         The column index of the maximum.
+    Raises
+    ------
+    ValueError
+        If the input is not a numpy error or
+        if the shape is not 2D.
+    """
+    i = 0
+    j = 0
 
+    # TODO
+    if type(X) != np.ndarray:
+        raise ValueError("X must be a np.array")
+    if len(X.shape) != 2:
+        raise ValueError("Input shape must be 2D")
+    d=np.where(X==np.max(X,axis=None))
+    i=d[0][0]
+    j=d[1][0]
+  
+    return i, j
+
+
+def wallis_product(n_terms):
+    """Implement the Wallis product to compute an approximation of pi.
+    See:
+    https://en.wikipedia.org/wiki/Wallis_product
+    XXX : write Parameters and Returns sections as above.
+    """
+    # XXX : The n_terms is an int that corresponds to the number of
+    # terms in the product. For example 10000.
+    return 0.
+# noqa: D100
+import numpy as np
+
+
+def max_index(X):
+    """Return the index of the maximum in a numpy array.
+    Parameters
+    ----------
+    X : ndarray of shape (n_samples, n_features)
+        The input array.
+    Returns
+    -------
+    i : int
+        The row index of the maximum.
+    j : int
+        The column index of the maximum.
     Raises
     ------
     ValueError
