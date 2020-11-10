d6c47d7e044abe730013a662cfe0065e7606e1ac
BertrandPaturel <paturel.bertrand@gmail.com>
2020-11-10 12:56:00 +0100

    write the comments

--- numpy_questions.py
+++ numpy_questions.py
@@ -1,4 +1,5 @@
 # noqa: D100
+import numpy as np
 
 
 def max_index(X):
@@ -27,11 +28,11 @@ def max_index(X):
     j = 0
 
     # TODO
-    # if type(X) != 'numpy.ndarray':
-    #    raise TypeError('X is not an array')
+    if not isinstance(X, np.ndarray):
+        raise ValueError('X is not an array')
 
     if len(X.shape) != 2:
-        raise TypeError('X is not a 2D vector')
+        raise ValueError('X is not a 2D vector')
 
     i = X.argmax(axis=0)
     j = X.argmax(axis=1)
@@ -45,15 +46,24 @@ def wallis_product(n_terms):
     See:
     https://en.wikipedia.org/wiki/Wallis_product
 
-    XXX : write Parameters and Returns sections as above.
+    Parameters
+    ----------
+    n_terms : int
+        The number of products to approximate pi.
+
+    Returns
+    -------
+    res : int
+        The wallis product of rank n_terms.
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
 
     i = 1
-    res = 1
+    pi = 2
 
     while i <= n_terms:
-        res = res * (2 * (i) ^ 2) / ((2 * (i) ^ 2) - 1)
+        pi = pi * (2 * (i) ^ 2) / ((2 * (i) ^ 2) - 1)
+        i += 1
 
-    return res
+    return pi
--- sklearn_questions.py
+++ sklearn_questions.py
@@ -7,24 +7,33 @@ from sklearn.utils.validation import check_array
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    """Write docstring
-    """
+    """This is the class of our estimator"""
+
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """Write docstring
-        """
+        """fit takes the training data and keep it in memory
+        through attributes.
+        ----
+        Parameters :
+        X : characteristic of the data train
+        y : targets of the trainin set."""
         X, y = check_X_y(X, y)
         self.classes_ = np.unique(y)
         # XXX fix
-        self.X = X
-        self.y = y
+        self.X = X.copy()
+        self.y = y.copy()
         return self
 
     def predict(self, X):
-        """Write docstring
-        """
+        """Gives the predict targets for a test set.
+        ----
+        Parameters :
+        X : Characteristics of the data set.
+        ----
+        Return :
+        y_pred : the predict set."""
         check_is_fitted(self)
         X = check_array(X)
         y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
@@ -33,8 +42,15 @@ class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
         return y_pred
 
     def score(self, X, y):
-        """Write docstring
-        """
+        """Gives the score for a data set between the prediction
+        made on this data set by the estimator and the real target
+        ----
+        Parameters :
+        X : the characteristics of the data set
+        y : the real tragets associated with the given data set X.
+        ----
+        Return :
+        the score (i.e. the average of good predictions)"""
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
         return np.mean(y_pred == y)
