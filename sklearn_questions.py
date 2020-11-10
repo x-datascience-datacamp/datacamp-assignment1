88e4cea052e62209ae8a8800973a26abd011b3cb
Pierre-André <pierreandremikem@gmail.com>
2020-11-10 08:18:05 +0100

    send modifications

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -3,33 +3,74 @@ import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
 from sklearn.utils.validation import check_array
+from sklearn.utils.multiclass import check_classification_targets
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    """Write docstring
-    """
+    """Class for one nearest neighbor model"""
+
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """Write docstring
         """
+        Fit the model.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training samples.
+        y : array-like of shape (n_samples,)
+            True labels for X.
+            
+        Returns
+        -------
+        score : object
+            One nearest neighbor instance.
+        """
+        check_classification_targets(y)
         X, y = check_X_y(X, y)
         self.classes_ = np.unique(y)
-        # XXX fix
+        self._X = X
+        self._y = y
         return self
 
     def predict(self, X):
-        """Write docstring
+        """
+        Returns the predictions of the model w.r.t the input.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training samples.
+            
+        Returns
+        -------
+        y_pred : array-like of shape (n_samples,)
+            Predicted labels.
         """
         check_is_fitted(self)
         X = check_array(X)
-        y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
-        # XXX fix
+        y_pred = np.full(shape=len(X), fill_value=self.classes_[0], dtype=self._y.dtype)
+        for i in range(len(y_pred)):
+            y_pred[i] = self._y[np.argmin(np.linalg.norm(self._X - X[i], axis=1))]
         return y_pred
 
     def score(self, X, y):
-        """Write docstring
+        """
+        Return the mean accuracy on the given test data and labels.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Test samples.
+        y : array-like of shape (n_samples,)
+            True labels for X.
+
+        Returns
+        -------
+        score : float
+            Mean accuracy of self.predict(X) wrt. y.
         """
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
