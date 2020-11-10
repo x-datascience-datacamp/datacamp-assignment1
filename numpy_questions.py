d9fdf853fe0e9716cf79467ca887d75db5be9350
ramufasa <ramy.merabet@ensta-paris.fr>
2020-11-10 11:09:53 +0100

    First Assignment

--- numpy_questions.py
+++ numpy_questions.py
@@ -4,50 +4,41 @@ import numpy as np
 
 def max_index(X):
     """Return the index of the maximum in a numpy array.
-
     Parameters
     ----------
     X : ndarray of shape (n_samples, n_features)
         The input array.
-
     Returns
     -------
     i : int
         The row index of the maximum.
-
     j : int
         The column index of the maximum.
-
     Raises
     ------
     ValueError
         If the input is not a numpy error or
         if the shape is not 2D.
     """
-    i = 0
-    j = 0
-
-    if X.type != "numpy.array":
-        raise ValueError("Not a numpy.array")
-
-    if np.shape(X) != 2 :
-        raise ValueError("Not a 2D array")
-
+    if type(X) is not np.ndarray:
+        raise ValueError('X is not a numpy array')
+    if len(X.shape) != 2:
+        raise ValueError('X is not a 2D array')  
+    
+    i, j = np.unravel_index(np.argmax(X, axis=None), X.shape)
     return i, j
 
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
-
     See:
     https://en.wikipedia.org/wiki/Wallis_product
-
     XXX : write Parameters and Returns sections as above.
-
     """
-    my_pi = 1
+    # XXX : The n_terms is an int that corresponds to the number of
+    # terms in the product. For example 10000.
 
-    for i in range(1, n_terms+1) :
-        my_pi *= 4*i**2/(4*i**2 - 1)
-
-    return 2*my_pi
+    pi = 1
+    for i in range(1, n_terms + 1):
+        pi *= 4 * i ** 2 / (4 * i ** 2 - 1)
+    return 2*pi
--- sklearn_questions.py
+++ sklearn_questions.py
@@ -1,36 +1,78 @@
 # noqa: D100
+
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
 from sklearn.utils.validation import check_array
+from sklearn.utils.multiclass import type_of_target
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    """Write docstring
-    """
+    """ 1-NearestNeightbor estimator."""
+
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """Write docstring
+        """ Fit method the 1-NearestNeightbor
+        Parameters
+        ---------       
+        X : ndarray of shape (n_samples, n_features)
+            The input array.
+        y : ndarray of shape (n_samples, 1)   
+        Returns 
+        --------
+        self : OneNearestNeighbor
         """
+
         X, y = check_X_y(X, y)
         self.classes_ = np.unique(y)
-        # XXX fix
+
+        if type_of_target(y) not in ['binary', 'multiclass', 'unknown']:
+            raise ValueError(f"Unknown label type: {y}")
+
+        self.X_train_ = X
+        self.y_train_ = y
         return self
 
     def predict(self, X):
-        """Write docstring
+        """Prediction of the class of X.
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            The input array.
+        Returns
+        -------
+        pred : ndarray of shape (len(X), 1)
+                The output array with the predictions.
         """
+
         check_is_fitted(self)
         X = check_array(X)
-        y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
-        # XXX fix
-        return y_pred
+        pred = np.full(shape=len(X), fill_value=self.classes_[0])
+        dist = np.zeros((len(X)))
+
+        for i in range(len(X)):
+            dist[i] = np.argmin(np.linalg.norm(self.X_train_-X[i], axis=1))
+        dist = dist.astype(int)
+
+        pred = self.y_train_[dist]
+
+        return pred
 
     def score(self, X, y):
-        """Write docstring
+        """Calculate the error rate of our predictor.
+            Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            The input array on which we will predict.
+        y : ndarray of shape (n_samples)
+            The input array with correct classes
+        Returns
+        -------
+        score : float 
+                The score of the prediction.
         """
         X, y = check_X_y(X, y)
-        y_pred = self.predict(X)
-        return np.mean(y_pred == y)
+        pred = self.predict(X)
+        return np.mean(pred == y)
