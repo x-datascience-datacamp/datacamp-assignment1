a6b23e18598749ae3cdc071c4037c35dfe4e1ee5
Romain Ilbert <romainilbert@MacBook-Pro-de-Romain-2.local>
2020-11-10 13:07:10 +0100

    numpy_ri

--- numpy_questions.py
+++ numpy_questions.py
@@ -26,21 +26,17 @@ def max_index(X):
     """
     i = 0
     j = 0
-
-    # TODO
-
-    return i, j
+    if type(X) is not np.ndarray:
+        raise ValueError('The input is not a np array')
+    if len(X.shape) != 2:
+        raise ValueError('X is not a np array')
+    (i, j) = np.unravel_index(np.argmax(X), X.shape)
+    return(i, j)
 
 
 def wallis_product(n_terms):
-    """Implement the Wallis product to compute an approximation of pi.
-
-    See:
-    https://en.wikipedia.org/wiki/Wallis_product
-
-    XXX : write Parameters and Returns sections as above.
-
-    """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    pi = 1
+    for i in range(1, n_terms+1):
+        pi *= 4*i**2 / (4*i**2-1)
+    pi *= 2
+    return pi
