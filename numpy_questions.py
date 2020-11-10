2ba8941f7a2e49f8b22388c5d64e26b76abf23a2
HUANG-KX <huangkx9708@gmail.com>
2020-11-10 19:58:19 +0800

    my_branch

--- numpy_questions.py
+++ numpy_questions.py
@@ -29,9 +29,9 @@ def max_index(X):
 
     # TODO
     if type(X) is not np.ndarray:
-        raise TypeError("Input is not numpy")
+        raise ValueError("Input is not numpy")
     if X.ndim != 2:
-        raise TypeError("The shape of input is not 2D")
+        raise ValueError("The shape of input is not 2D")
 
     i, j = np.unravel_index(np.argmax(X, axis=None), X.shape)
 
@@ -49,8 +49,10 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    out = 1
-    for i in range(1, n_terms+1):
-        out = 4*(n_terms+1)**2/(4*(n_terms+1)**2-1)
+    out = 2
 
+    if n_terms == 0:
+        return out
+    for i in range(1, n_terms+1):
+        out = out * 4 * (i)**2 / (4 * (i)**2 - 1)
     return out
--- sklearn_questions.py
+++ sklearn_questions.py
@@ -3,34 +3,37 @@ import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
 from sklearn.utils.validation import check_array
+from sklearn.utils.multiclass import check_classification_targets
+from sklearn.metrics.pairwise import euclidean_distances
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    """Write docstring
-    """
+    """Use the nearest neighbor to do the classification."""
+
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """Write docstring
-        """
+        """Use the data to fit the model."""
         X, y = check_X_y(X, y)
+        self.n_features_in_ = X.shape[1]
+        check_classification_targets(y)
         self.classes_ = np.unique(y)
-        # XXX fix
+        self.X_ = X
+        self.y_ = y
         return self
 
     def predict(self, X):
-        """Write docstring
-        """
+        """Predict the cluster of X.""" 
         check_is_fitted(self)
         X = check_array(X)
         y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
-        # XXX fix
+        distance = euclidean_distances(X, self.X_)
+        y_pred = self.y[np.argmin(distance, axis=1)]
         return y_pred
 
     def score(self, X, y):
-        """Write docstring
-        """
+        """Compute mean accuracy."""
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
         return np.mean(y_pred == y)
