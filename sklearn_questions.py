c578321d97f6be0d882c5d9a258cb51ec0f782c9
mohamedbabana <44004963+mohamedbabana@users.noreply.github.com>
2020-11-10 01:00:35 +0100

    Update sklearn_questions.py

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -2,35 +2,44 @@
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted
+from sklearn.utils.multiclass import check_classification_targets
 from sklearn.utils.validation import check_array
 
 
 class OneNearestNeighbor(BaseEstimator, ClassifierMixin):
-    """Write docstring
-    """
+    """One Nearest Neighbor class.""" 
+
     def __init__(self):  # noqa: D107
         pass
 
     def fit(self, X, y):
-        """Write docstring
-        """
+        """Fit the data."""
         X, y = check_X_y(X, y)
+        check_classification_targets(y)
         self.classes_ = np.unique(y)
         # XXX fix
+        self.X_ = X
+        self.y_ = y
+
         return self
 
     def predict(self, X):
-        """Write docstring
-        """
+        """Predict the classes."""
         check_is_fitted(self)
         X = check_array(X)
-        y_pred = np.full(shape=len(X), fill_value=self.classes_[0])
+        y_pred = np.full(shape=len(X), fill_value=self.classes_[0],
+                         dtype = self.classes_.dtype)
         # XXX fix
+        for i in range(y_pred.shape[0]):
+            euclidean_distances = np.linalg.norm(self.X_ - X[i,:], axis = 1)
+            index_closest = np.argmin(euclidean_distances)
+            y_pred[i] = self.y_[index_closest]
+            
         return y_pred
 
     def score(self, X, y):
-        """Write docstring
-        """
+        """Computes the score."""
         X, y = check_X_y(X, y)
         y_pred = self.predict(X)
         return np.mean(y_pred == y)
+
